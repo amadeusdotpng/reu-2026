f4a60e2a749b561b8d4d3496773a1adc6b99aa40
amadeusdotpng <amadeus8182@gmail.com>
2026-03-20 02:22:45 -0500

    initial commit

---
+++ exercise_3_start_at.py
@@ -0,0 +1,9 @@
+from math import ceil
+
+def f(rows: int):
+    return (
+        f'start at: {2**(rows.bit_length()-1) - ceil(rows / 2):0>{rows.bit_length()-1}b}',
+        f'copy seed?: {rows % 2 == 1}',
+    )
+
+print(f'rows = 1000: {f(1000)}')
